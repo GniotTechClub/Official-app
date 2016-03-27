690f93b9f86b3b9dacf1cd345356340f138d251c
ashu94 <singhjug1994@gmail.com>
2016-03-27 23:34:22 +0530

    initialize the app

---
+++ Gniot_tech_club/app/src/test/java/techclub/gniot/juggernaut/gniot_tech_club/ExampleUnitTest.java
@@ -0,0 +1,15 @@
+package techclub.gniot.juggernaut.gniot_tech_club;
+
+import org.junit.Test;
+
+import static org.junit.Assert.*;
+
+/**
+ * To work on unit tests, switch the Test Artifact in the Build Variants view.
+ */
+public class ExampleUnitTest {
+    @Test
+    public void addition_isCorrect() throws Exception {
+        assertEquals(4, 2 + 2);
+    }
+}
